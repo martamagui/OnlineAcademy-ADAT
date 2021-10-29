c65b9816bd32df8e0485299275629e918118ecf6
Marta Molina <marta.molina@live.u-tad.com>
2021-10-29 19:16:55 +0200

    Trying select queries with Php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "OnlineAcademy";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "SELECT * FROM Courses";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Title: " . $row["title"]. " - Teacher: " . $row["teacher"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+
+$conn->close();
+?>
