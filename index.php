d3ed979f0e947f09b064bacc30a0ba959973ac44
Marta Molina <marta.molina@live.u-tad.com>
2021-10-29 19:57:37 +0200

    New inserts

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $dbname = "OnlineAcademy";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // sql to create table
@@ -17,13 +17,10 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "Title: " . $row["title"]. " - Teacher: " . $row["teacher"]. "<br>";
+    while ($row = $result->fetch_assoc()) {
+        echo "Title: " . $row["title"] . " - Teacher: " . $row["teacher"] . "<br>";
     }
-  } else {
+} else {
     echo "0 results";
-  }
-  $conn->close();
-
+}
 $conn->close();
-?>
