dda39f3c21d59e427cae47000f2a988d600d0649
Marta Molina <marta.molina@live.u-tad.com>
2021-11-04 20:01:15 +0100

    Starting the website

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <p>Esto es el footer</p>
+</footer>
+<script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>OnlineCraftAcademy</title>
+</head>
+
+<body>
+    <nav>
+        <!--NavBar-->
+        <div class="nav">
+            <div id="navName">
+
+                <span id="nav-name">OnlineCraftAcademy</span>
+            </div>
+            <ul class="nav__list" id="nav-list">
+                <li class="nav__item d-occult" id="nav-about">
+                    <a href="#about">Cursos</a>
+                    <div class="nav_dropdown">
+                        <p>Categorías</p>
+                        <ul>
+                            <li><a href="#">Ilustración</a></li>
+                            <li><a href="#">Punto y Bordado</a></li>
+                            <li><a href="#">Reciclaje</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav__item d-occult" id="nav-work">
+                    <a href="#work">Work</a>
+                </li>
+                <li class="nav__item d-occult" id="nav-experience">
+                    <a href="#experience">Experience</a>
+                </li>
+                <li class="nav__item d-occult" id="nav-contact">
+                    <a href="#contact">Contact</a>
+                </li>
+                <li class="nav__item d-occult" id="nav-resume">
+                    <a href="#">Resume</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
