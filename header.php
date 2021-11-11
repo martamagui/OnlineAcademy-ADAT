847ae49d6cb767f21a4a993b45e251003a9250bd
martamagui <marta.molina@live.u-tad.com>
2021-11-11 13:14:03 +0100

    What I did last friday

--- header.php
+++ header.php
@@ -22,20 +22,21 @@
                     <div class="nav__item collapse-submenu">
                         <p class="collapse__tiltle">Categorías</p>
                         <ul>
-                            <li class="collapse__item"><a href="#">Ilustración</a></li>
-                            <li class="collapse__item"><a href="#">Punto y Bordado</a></li>
-                            <li class="collapse__item"><a href="#">Reciclaje</a></li>
+                            <li class="collapse__item d-occult"><a href="#">Ilustración</a></li>
+                            <li class="collapse__item d-occult"><a href="#">Punto y Bordado</a></li>
+                            <li class="collapse__item d-occult"><a href="#">Reciclaje</a></li>
                         </ul>
                     </div>
                 </li>
-                <li class="nav__item d-occult" id="nav-contact">
-                    <a href="#" id="shopping-cart"><img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-shopping-cart-miscellaneous-kiranshastry-gradient-kiranshastry.png" /></a>
+                <li class="nav__item " id="nav-shopping-cart">
+                    <a href="#" id="shopping-cart"><img id="nav-shopping-cart-img"src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-shopping-cart-miscellaneous-kiranshastry-gradient-kiranshastry.png" /></a>
                 </li>
-                <li class="nav__item d-occult" id="nav-experience">
-                    <a href="#" id="login">Entrar</a>
+                <li class="nav__item" id="nav-experience">
+                    
+                    <a href="#" id="login"><button class="button-dark">Entrar</button></a>
                 </li>
-                <li class="nav__item d-occult" id="nav-experience">
-                    <a href="#" id="login">Crear cuenta</a>
+                <li class="nav__item " id="nav-experience">
+                    <a href="#" id="login"><button class="button-light">Crear cuenta</button></a>
                 </li>
             </ul>
         </div>
--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 include_once 'header.php'
 ?>
-<p>Esto es la main section.</p>
+<div class="banner" id="banner">
+    <div class="banner__column">
+        <img src="img/banner.jpg" alt="Artwork Paula Bonnnet">
+    </div>
+    <div class="banner__column">
+        <h2 class="banner__title banner__item">Libera tu creatividad con OnlineCraftAcademy.</h2>
+        <span class="banner__item"> Aprende de los mejores artistas de cada área.</span>
+        <div class="banner__button__container banner__item">
+            <button class="banner__button button-dark">Ver cursos</button>
+            <button class="banner__button button-light">Crea tu cuenta</button>
+        </div>
+    </div>
+
+</div>
 <?php
 include_once 'footer.php'
 ?>
