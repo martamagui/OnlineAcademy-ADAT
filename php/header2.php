3ce34f8880a9b4eabdd8e1d9785438981bf1bcf6
martamagui <marta.molina@live.u-tad.com>
2021-12-04 20:27:02 +0100

    new font

--- php/header2.php
+++ php/header2.php
@@ -7,13 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/main.css">
     <title>OnlineCraftAcademy</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@500&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <nav>
         <!--NavBar-->
         <div class="nav">
-            <div id="navName"> 
+            <div id="navName">
                 <a href="../index.php"><span id="nav-name">OnlineCraftAcademy</span></a>
             </div>
             <ul class="nav__list" id="nav-list">
@@ -29,14 +32,14 @@
                     </div>
                 </li>
                 <li class="nav__item" id="nav-experience">
-                    
+
                     <a href="../login_form.php" id="login"><button class="button-dark basic__button">Entrar</button></a>
                 </li>
                 <li class="nav__item " id="nav-experience">
                     <a href="../signup_form.php" id="login"><button class="button-light basic__button">Crear cuenta</button></a>
                 </li>
                 <li class="nav__item " id="nav-shopping-cart">
-                    <a href="../shopping_cart.php" id="shopping-cart"><img id="nav-shopping-cart-img"src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-shopping-cart-miscellaneous-kiranshastry-gradient-kiranshastry.png" /></a>
+                    <a href="../shopping_cart.php" id="shopping-cart"><img id="nav-shopping-cart-img" src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-shopping-cart-miscellaneous-kiranshastry-gradient-kiranshastry.png" /></a>
                 </li>
             </ul>
         </div>
