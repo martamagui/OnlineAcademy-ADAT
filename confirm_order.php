84be24addbc46354d4dba9d2d392769ef97de86b
martamagui <marta.molina@live.u-tad.com>
2021-12-10 11:51:23 +0100

    Button and links hover

--- confirm_order.php
+++ confirm_order.php
@@ -1,7 +1,12 @@
 <?php
 include_once 'header.php'
 ?>
-<h1>Gracias por tu pedido.</h1>
+
+<div class="banner">
+<div class="banner__column"><img src="img/tyOrder.jpg" alt="Thanks image"></div>
+<div class="banner__column"><h1>Gracias por tu pedido.</h1></div>
+</div>
+
 <?php
 include_once 'footer.php'
 ?>
