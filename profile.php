c47a71d7b7adc032f93e3a81f24e590772491899
Marta Molina <marta.molina@live.u-tad.com>
2021-12-12 23:22:26 +0100

    Nice, nice

--- profile.php
+++ profile.php
@@ -11,23 +11,23 @@ include_once 'header.php';
 
     <div class="edit__container">
         <h3>Editar información.</h3>
-        <form action="php/user/update_name.php" method="post">
-            <label for="email">Nombre</label>
-            <input type="text" placeholder="Nombre" name="firstName" id="firstName">
+        <form action="php/user/update_name.php" method="post" class="form__container">
+            <label for="email" class="form__item">Nombre</label>
+            <input type="text" class="form__item" placeholder="Nombre" name="firstName" id="firstName">
 
-            <label for="email">Apellidos:</label>
-            <input type="text" placeholder="Apellidos" name="lastName" id="lastName">
+            <label for="email" class="form__item">Apellidos:</label>
+            <input type="text" class="form__item" placeholder="Apellidos" name="lastName" id="lastName">
 
-            <input type="submit" value="Actualizar datos">
+            <input type="submit" value="Actualizar datos" class="basic__button button-dark">
         </form>
-        <form action="php/user/update_psw.php" method="post">
-            <label for="psw">Contraseña: </label>
-            <input type="password" placeholder="Contraseña" name="password" id="password" required>
+        <form action="php/user/update_psw.php" method="post" class="form__container">
+            <label for="psw" class="form__item">Contraseña: </label>
+            <input type="password" class="form__item" placeholder="Contraseña" name="password" id="password" required>
 
-            <label for="psw-repeat">Repita la contraseña: </label>
-            <input type="password" placeholder="Repita Contraseña" name="psw-repeat" id="psw-repeat" required>
+            <label for="psw-repeat" class="form__item">Repita la contraseña: </label>
+            <input type="password" class="form__item" placeholder="Repita Contraseña" name="psw-repeat" id="psw-repeat" required>
 
-            <input type="submit" value="Cambiar contraseña">
+            <input type="submit" value="Cambiar contraseña" class="basic__button button-dark">
         </form>
     </div>
     <div class="orders__container">
