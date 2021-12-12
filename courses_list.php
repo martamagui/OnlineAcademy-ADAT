092f551fa65513bee90494ff92135d3bb1251db4
Marta Molina <marta.molina@live.u-tad.com>
2021-12-12 18:37:42 +0100

    Commit

--- courses_list.php
+++ courses_list.php
@@ -1,29 +1,31 @@
 <?php
 include_once 'header.php'
 ?>
-<div class="wrapper">
-    <form action="courses_list.php" method="post">
-        <h4>Filtrar por categoría:</h4>
-        <select name="category" id="select-category">
-            <option value="All">Todos</option>
-            <option value="Ilustración">Ilustración</option>
-            <option value="Reciclaje">Reciclaje</option>
-            <option value="Punto y Bordado">Punto y Bordado</option>
-        </select>
-        <h4>Ordenar por:</h4>
-        <select name="orderBy" id="select-order">
-            <option value="none">Relevancia</option>
-            <option value="title">Orden álfabetico</option>
-            <option value="price">Precio ascendente</option>
-            <option value="priceDesc">Precio descendente</option>
-        </select>
-        <button type="submit" class="button-dark basic__button">Mostrar resultados</button>
-    </form>
-</div>
-<div class='products__container'>
-    <?php
-    include_once 'php/list_component.php'
-    ?>
+<div class="container__products--and--filters">
+    <div class="wrapper">
+        <form action="courses_list.php" method="post">
+            <h4>Filtrar por categoría:</h4>
+            <select name="category" id="select-category">
+                <option value="All">Todos</option>
+                <option value="Ilustración">Ilustración</option>
+                <option value="Reciclaje">Reciclaje</option>
+                <option value="Punto y Bordado">Punto y Bordado</option>
+            </select>
+            <h4>Ordenar por:</h4>
+            <select name="orderBy" id="select-order">
+                <option value="none">Relevancia</option>
+                <option value="title">Orden álfabetico</option>
+                <option value="price">Precio ascendente</option>
+                <option value="priceDesc">Precio descendente</option>
+            </select>
+            <button type="submit" class="button-dark basic__button">Mostrar resultados</button>
+        </form>
+    </div>
+    <div class='products__container'>
+        <?php
+        include_once 'php/list_component.php'
+        ?>
+    </div>
 </div>
 <?php
 include_once 'footer.php'
--- php/load_6_products.php
+++ php/load_6_products.php
@@ -3,18 +3,37 @@
     require 'connection.php';
     $sql = "";
     $sql = "SELECT * FROM Courses";
-    $result = mysqli_query($connection, $sql) or die("Ups there was a conecction problem :S");
+    $result2 = mysqli_query($connection, $sql) or die("Ups there was a conecction problem :S");
 
-    if ($result->num_rows > 0) {
+    $cursosComprados = array();
+    if (isset($_SESSION["email"])) {
+        $user = $_SESSION["email"];
+        $result = $connection->query("SELECT * FROM Users WHERE  email='" . $user . "' ");
+        if ($result !== false && $result->num_rows > 0) {
+            $resultCourses = $connection->query("SELECT * FROM UserCourses WHERE emailFk='" . $user . "' ");
+            if ($resultCourses !== false && $resultCourses->num_rows > 0) {
+                while ($row = $resultCourses->fetch_assoc()) {
+                    array_push($cursosComprados, $row["courseIDfk"]);
+                }
+            }
+        }
+    }
+
+    if ($result2->num_rows > 0) {
         for ($i = 0; $i < 6; $i++) {
-            $row = $result->fetch_assoc();
+            $row = $result2->fetch_assoc();
             echo "<form action='php/getProduct.php?course=" . $row["courseID"] . "' method='post' class ='product__card'>";
             echo "<a href='product_detail.php?course=" . $row["courseID"] . "'><img src='img/" . $row["imgName"] . "' /></a>";
             echo "<div class ='product__card__info'>";
             echo "<h2>" . $row["title"] . "</h2>";
             echo "<span> Curso impartido por " . $row["teacher"] . "</span> ";
             echo "<a href='showCategory.php?value=" . $row["category"] . "'>" . $row["category"] . "</a>";
-            echo "<input type='submit' value='" . number_format((float)$row["price"], 2, '.', '') . " €' class='basic__button button-dark''/>";
+            echo "<input type='submit' value='";
+            if (in_array($row["courseID"], $cursosComprados)) {
+                echo "Ya en propiedad' disabled class='basic__button button-disabled'/>";
+            } else {
+                echo  number_format((float)$row["price"], 2, '.', '') . " €' class='basic__button button-dark''/>";
+            }
             echo "</div>";
             echo "</form>";
         }
