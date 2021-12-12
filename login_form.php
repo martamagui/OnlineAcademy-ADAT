d1142d3a2c8a589e4d4ed9b8505d6422457dfaf3
Marta Molina <marta.molina@live.u-tad.com>
2021-12-13 00:40:18 +0100

    Sign up styling done :D

--- login_form.php
+++ login_form.php
@@ -1,18 +1,18 @@
 <?php
 include_once 'header.php'
 ?>
-<div class="login__form-popup hidden" id="myForm">
-  <form action="php/login_action.php" method="post" class="login__container" id="login">
-    <h2>Login</h2>
+<div class="login__form hidden edit__container" id="myForm">
+  <form action="php/login_action.php" class="form__container" method="post" class="login__container" id="login">
+    <h2 class="form__item">Login</h2>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Email" name="email" required>
+    <label for="email" class="form__item"><b>Email</b></label>
+    <input type="text" placeholder="Email" name="email" required class="form__item">
 
-    <label for="psw"><b>Contraseña</b></label>
-    <input type="password" placeholder="Contraseña" name="password" required>
+    <label class="form__item" for="psw"><b>Contraseña</b></label>
+    <input class="form__item" type="password" placeholder="Contraseña" name="password" required>
 
-    <button type="submit" class="basic__button button-dark">Login</button>
-    <p>¿Aún no tienes cuenta? <a href="signup_form.php">Crear cuenta</a></p>
+    <button type="submit" class="basic__button button-dark form__item">Login</button>
+    <p class="form__item">¿Aún no tienes cuenta? <a href="signup_form.php">Crear cuenta</a></p>
   </form>
 </div>
 
--- signup_form.php
+++ signup_form.php
@@ -1,32 +1,32 @@
 <?php
 include_once 'header.php'
 ?>
-<div class="signup__form-popup hidden">
+<div class="signup__form hidden">
   <form action="php/signup_action.php" method="post">
     <h2>Crear nueva cuenta</h2>
-    <p>Please fill in this form to create an account.</p>
 
-    <label for="email">¿Cómo te llamas?</label>
-    <input type="text" placeholder="Nombre" name="firstName" id="firstName" required>
+    <label for="email" class="form__item">¿Cómo te llamas?</label>
+    <input type="text" placeholder="Nombre" name="firstName" id="firstName" class="form__item" required>
 
-    <label for="email">Apellidos:</label>
-    <input type="text" placeholder="Apellidos" name="lastName" id="lastName" required>
+    <label for="email" class="form__item">Apellidos:</label>
+    <input type="text" placeholder="Apellidos" name="lastName" id="lastName" class="form__item" required>
 
-    <label for="email">Email: </label>
-    <input type="text" placeholder="Enter Email" name="email" id="email" required>
+    <label for="email" class="form__item">Email: </label>
+    <input type="text" placeholder="Enter Email" name="email" id="email" class="form__item" required>
 
-    <label for="psw">Contraseña: </label>
-    <input type="password" placeholder="Enter Password" name="password" id="password" required>
+    <label for="psw" class="form__item">Contraseña: </label>
+    <input class="form__item" type="password" placeholder="Enter Password" name="password" id="password" required>
 
-    <label for="psw-repeat">Repita la contraseña: </label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+    <label for="psw-repeat" class="form__item">Repita la contraseña: </label>
+    <input class="form__item" type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
 
-    <button type="submit" class="button-dark basic__button">Registrarme</button>
+    <button type="submit" class="button-dark basic__button form__item">Registrarme</button>
   </form>
-</div>
-<div class="container signin">
-  <p>Already have an account? <a href="#">Sign in</a>.</p>
-  <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+
+  <div class="container__signin">
+    <p class="form__item">Already have an account? <a href="login_form.php">Sign in</a>.</p>
+    <p class="form__item">By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+  </div>
 </div>
 <?php
 include_once 'footer.php'
