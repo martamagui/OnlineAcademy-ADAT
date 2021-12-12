9a32318c04c0c2d79c88b842bb0944a73a6f4520
Marta Molina <marta.molina@live.u-tad.com>
2021-12-12 18:47:36 +0100

    Filter Style

--- courses_list.php
+++ courses_list.php
@@ -4,20 +4,24 @@ include_once 'header.php'
 <div class="container__products--and--filters">
     <div class="wrapper">
         <form action="courses_list.php" method="post">
-            <h4>Filtrar por categoría:</h4>
-            <select name="category" id="select-category">
-                <option value="All">Todos</option>
-                <option value="Ilustración">Ilustración</option>
-                <option value="Reciclaje">Reciclaje</option>
-                <option value="Punto y Bordado">Punto y Bordado</option>
-            </select>
-            <h4>Ordenar por:</h4>
-            <select name="orderBy" id="select-order">
-                <option value="none">Relevancia</option>
-                <option value="title">Orden álfabetico</option>
-                <option value="price">Precio ascendente</option>
-                <option value="priceDesc">Precio descendente</option>
-            </select>
+            <div class="courses__filter">
+                <h4>Filtrar por categoría:</h4>
+                <select name="category" id="select-category">
+                    <option value="All">Todos</option>
+                    <option value="Ilustración">Ilustración</option>
+                    <option value="Reciclaje">Reciclaje</option>
+                    <option value="Punto y Bordado">Punto y Bordado</option>
+                </select>
+            </div>
+            <div class="courses__filter">
+                <h4>Ordenar por:</h4>
+                <select name="orderBy" id="select-order">
+                    <option value="none">Relevancia</option>
+                    <option value="title">Orden álfabetico</option>
+                    <option value="price">Precio ascendente</option>
+                    <option value="priceDesc">Precio descendente</option>
+                </select>
+            </div>
             <button type="submit" class="button-dark basic__button">Mostrar resultados</button>
         </form>
     </div>
